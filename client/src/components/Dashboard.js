90136737983f92ecef87546cc88a1ebe307db33b
DAMoreira <sk4rfac3@gmail.com>
2023-07-28 16:28:50 -0300

    Revert "la busqueda por fecha se setea por defecto en hoy"
    This reverts commit 8c025dbe91ba27fd215139616173e385c5eb5a48.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -23,8 +23,10 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import Spinner from "../images/loading.gif";
 import ReactPaginate from "react-paginate";
+
+// Importa los componentes del DatePicker de antd
 import { DatePicker } from "antd";
-import moment from "moment";
+//import "antd/dist/antd.css";
 
 const styles = (theme) => ({
   dashboard: {
@@ -62,14 +64,14 @@ class Dashboard extends Component {
     this.state = {
       appointments: [],
       filterName: "",
-      filterDate: moment(), // Establece la fecha de hoy como valor inicial
+      filterDate: null, // Cambiar el valor inicial a null
       appointment: {},
       loading: false,
       isAuthenticated: false,
       deleteModalOpen: false,
       editModalOpen: false,
       currentPage: 0,
-      appointmentsPerPage: 10,
+      appointmentsPerPage: 10, // Número de citas por página
     };
     this.dateInput = React.createRef();
     this.timeInput = React.createRef();
@@ -185,15 +187,25 @@ class Dashboard extends Component {
       filterDate,
       loading,
       appointment,
-      appointments,
       currentPage,
       appointmentsPerPage,
     } = this.state;
     const { fullname, date, time } = appointment;
 
+    // Filtrar por fecha antes de aplicar la paginación
+    const filteredAppointments = this.state.appointments
+      .filter((key) =>
+        filterDate
+          ? key.date.includes(filterDate.format("YYYY-MM-DD"))
+          : true
+      )
+      .filter((key) =>
+        key.fullname.toLowerCase().includes(filterName)
+      );
+
     const indexOfLastAppointment = (currentPage + 1) * appointmentsPerPage;
     const indexOfFirstAppointment = indexOfLastAppointment - appointmentsPerPage;
-    const currentAppointments = appointments.slice(
+    const currentAppointments = filteredAppointments.slice(
       indexOfFirstAppointment,
       indexOfLastAppointment
     );
@@ -209,7 +221,7 @@ class Dashboard extends Component {
             style={{ color: "#c9c9c9" }}
             gutterBottom
           >
-            Turnos Totales: {appointments.length}
+            Turnos Totales: {filteredAppointments.length}
           </Typography>
         </Box>
         <Box className={classes.searchInputs}>
@@ -255,51 +267,40 @@ class Dashboard extends Component {
                 </TableRow>
               </TableHead>
               <TableBody style={{ backgroundColor: "#c9c9c9" }}>
-                {currentAppointments
-                  .filter((key) =>
-                    key.fullname.toLowerCase().includes(filterName)
-                  )
-                  .filter((key) =>
-                    filterDate
-                      ? key.date.includes(
-                          filterDate.format("YYYY-MM-DD")
-                        )
-                      : true
-                  )
-                  .map((appointment) => (
-                    <TableRow key={appointment._id}>
-                      <TableCell>{nr++}</TableCell>
-                      <TableCell>{appointment.fullname}</TableCell>
-                      <TableCell>{appointment.cellphone}</TableCell>
-                      <TableCell>{appointment.date}</TableCell>
-                      <TableCell>{appointment.time}</TableCell>
-                      <TableCell>{appointment.description}</TableCell>
-                      <TableCell>
-                        <Button
-                          variant="contained"
-                          color="secondary"
-                          startIcon={<DeleteIcon />}
-                          onClick={() => {
-                            this.setState({ appointment });
-                            this.handleDeleteModalOpen();
-                          }}
-                        >
-                          Cancelar
-                        </Button>{" "}
-                        <Button
-                          variant="contained"
-                          color="primary"
-                          startIcon={<EditIcon />}
-                          onClick={() => {
-                            this.setState({ appointment });
-                            this.handleEditModalOpen();
-                          }}
-                        >
-                          Editar
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  ))}
+                {currentAppointments.map((appointment) => (
+                  <TableRow key={appointment._id}>
+                    <TableCell>{nr++}</TableCell>
+                    <TableCell>{appointment.fullname}</TableCell>
+                    <TableCell>{appointment.cellphone}</TableCell>
+                    <TableCell>{appointment.date}</TableCell>
+                    <TableCell>{appointment.time}</TableCell>
+                    <TableCell>{appointment.description}</TableCell>
+                    <TableCell>
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        startIcon={<DeleteIcon />}
+                        onClick={() => {
+                          this.setState({ appointment });
+                          this.handleDeleteModalOpen();
+                        }}
+                      >
+                        Cancelar
+                      </Button>{" "}
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        startIcon={<EditIcon />}
+                        onClick={() => {
+                          this.setState({ appointment });
+                          this.handleEditModalOpen();
+                        }}
+                      >
+                        Editar
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
               </TableBody>
             </Table>
           </TableContainer>
@@ -311,7 +312,7 @@ class Dashboard extends Component {
             nextLabel={">"}
             breakLabel={"..."}
             breakClassName={"break-me"}
-            pageCount={Math.ceil(appointments.length / appointmentsPerPage)}
+            pageCount={Math.ceil(filteredAppointments.length / appointmentsPerPage)}
             marginPagesDisplayed={2}
             pageRangeDisplayed={5}
             onPageChange={this.handlePageChange}
@@ -321,92 +322,7 @@ class Dashboard extends Component {
           />
         </Box>
 
-        <Modal
-          open={this.state.deleteModalOpen}
-          onClose={this.handleModalClose}
-        >
-          <div className={classes.modalContent}>
-            <Typography variant="h6" color="primary">
-              Deleting Appointment
-            </Typography>
-            <Typography variant="h6">
-              Are you sure you want to delete appointment with client:
-            </Typography>
-            <Typography variant="h5">{fullname}</Typography>
-            <Box mt={2}>
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={() => {
-                  this.handleModalClose();
-                  this.deleteAppointment(appointment._id);
-                }}
-                className={classes.modalButtons}
-              >
-                Yes
-              </Button>
-              <Button
-                variant="contained"
-                onClick={this.handleModalClose}
-                className={classes.modalButtons}
-              >
-                No
-              </Button>
-            </Box>
-          </div>
-        </Modal>
-        <Modal open={this.state.editModalOpen} onClose={this.handleModalClose}>
-          <div className={classes.modalContent}>
-            <Typography variant="h6" color="primary">
-              Editing Appointment
-            </Typography>
-            <Typography variant="h5" color="secondary">
-              Client: {fullname}
-            </Typography>
-            <form>
-              <Box mt={2}>
-                <TextField
-                  id="editDate"
-                  name="editDate"
-                  type="date"
-                  variant="outlined"
-                  defaultValue={date}
-                  inputRef={this.dateInput}
-                />
-              </Box>
-              <Box mt={2}>
-                <TextField
-                  id="editTime"
-                  name="editTime"
-                  type="time"
-                  variant="outlined"
-                  inputRef={this.timeInput}
-                  defaultValue={time}
-                />
-              </Box>
-              <Box mt={2}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={() => {
-                    this.handleModalClose();
-                    this.editAppointment();
-                  }}
-                  className={classes.modalButtons}
-                >
-                  Submit
-                </Button>
-                <Button
-                  variant="contained"
-                  onClick={this.handleModalClose}
-                  className={classes.modalButtons}
-                >
-                  Back
-                </Button>
-              </Box>
-            </form>
-          </div>
-        </Modal>
+        {/* Resto del código sin cambios */}
       </Container>
     );
   }
