b07fc0ba12580cb2f718f2e7faca3e9e34591c4c
Mariano <mgondolesi@gmail.com>
2023-07-31 14:34:03 -0300

    encripto contraseña en back

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
 
 
 // Controlador para crear un nuevo usuario
@@ -15,12 +17,63 @@ const addUser = (req, res) => {
     User.findOne({ username: lowercaseUsername }).then((user) => {
       if (user) return res.status(400).json({ msg: "Username already exists." });
   
-      const newUser = new User({
-        username: lowercaseUsername, // Almacena el username en minúsculas
-        password,
-      });
+      // Encriptar la contraseña utilizando bcrypt
+      bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({ msg: "Error occurred while hashing password." });
+        }
   
-      newUser.save().then((user) => {
+        const newUser = new User({
+          username: lowercaseUsername, // Almacena el username en minúsculas
+          password: hash, // Almacena la contraseña encriptada
+        });
+  
+        newUser.save().then((user) => {
+          jwt.sign(
+            { id: user._id },
+            process.env.jwtSecret,
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) return res.status(400).json({ msg: "Something went wrong" });
+              res.json({
+                token,
+                user: {
+                  id: user._id,
+                  username: user.username,
+                },
+              });
+            }
+          );
+        });
+      });
+    });
+  };
+  
+  
+
+// Controlador para loguear un usuario
+const loginUser = (req, res) => {
+    const { username, password } = req.body;
+  
+    if (!username || !password) {
+      return res.status(400).json({ msg: "Please enter all fields." });
+    }
+  
+    const lowercaseUsername = username.toLowerCase(); // Convertir a minúsculas
+  
+    User.findOne({ username: lowercaseUsername }).then((user) => {
+      if (!user) return res.status(400).json({ msg: "User does not exist." });
+  
+      // Comparar la contraseña proporcionada con la contraseña encriptada almacenada en la base de datos
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) {
+          return res.status(500).json({ msg: "Error occurred while comparing passwords." });
+        }
+  
+        if (!isMatch) {
+          return res.status(400).json({ msg: "Invalid credentials" });
+        }
+  
         jwt.sign(
           { id: user._id },
           process.env.jwtSecret,
@@ -37,41 +90,6 @@ const addUser = (req, res) => {
           }
         );
       });
-    });
-  };
-  
-
-// Controlador para loguear un usuario
-const loginUser = (req, res) => {
-    const { username, password } = req.body;
-  
-    if (!username || !password) {
-      return res.status(400).json({ msg: "Please enter all fields." });
-    }
-  
-    const lowercaseUsername = username.toLowerCase(); // Convertir a minúsculas
-  
-    User.findOne({ username: lowercaseUsername }).then((user) => {
-      if (!user) return res.status(400).json({ msg: "User does not exist." });
-  
-      if (user.password !== password)
-        return res.status(400).json({ msg: "Invalid credentials" });
-  
-      jwt.sign(
-        { id: user._id },
-        process.env.jwtSecret,
-        { expiresIn: 3600 },
-        (err, token) => {
-          if (err) return res.status(400).json({ msg: "Something went wrong" });
-          res.json({
-            token,
-            user: {
-              id: user._id,
-              username: user.username,
-            },
-          });
-        }
-      );
     });
   };
   
